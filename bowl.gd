bcdca40ffce37e99ce4347450a46642e25795e27
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-31 16:46:06 +0100

    add a pretty cool bowl that you can put candy in

---
+++ bowl.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@onready var area = $Area3D
+@onready var visibility = $VisibleOnScreenNotifier3D
+@onready var model = $CandyGhost
+
+@export var player: Node3D
+
+var could_interact = false
+var carried = false
+var full = false
+
+func _process(_delta: float) -> void:
+	model.visible = player.carrying != null and not full and player.carrying.identifier == "candy_bag"
+	if !model.visible:
+		return
+	var overlapping = area.get_overlapping_bodies() 
+	var can_interact = overlapping and visibility.is_on_screen()
+	if can_interact and !could_interact:
+		ui.show_interact()
+	elif !can_interact and could_interact:
+		ui.hide_interact()
+	could_interact = can_interact
+	if can_interact and Input.is_action_just_pressed("interact"):
+		var candy_bag = player.carrying
+		full = true
+		player.carrying = null
+		candy_bag.queue_free()
+		$Candy.visible = true
+		
